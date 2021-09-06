8b393b2d3adcbfff481feb99afd2630dd4604927
29rpl4aulfeb <72597402+29rpl4aulfeb@users.noreply.github.com>
2021-09-06 11:19:15 +0700

    Add files via upload

---
+++ Modul 1/latihan1.php
@@ -0,0 +1,5 @@
+<?php
+echo "Namaku Aulia";
+echo "<br>";
+echo "Aku sedang belajar PHP di SMK Telkom Malang";
+?>
---
+++ Modul 1/latihan4.php
@@ -0,0 +1,8 @@
+<?php
+$nama="Aulia";
+function tampil_nama(){
+    global $nama;
+    echo "Nama Saya : ".$nama; 
+}
+tampil_nama();
+?>
